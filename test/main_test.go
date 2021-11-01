272e9125eda5c701a2729ab4594e398a77725d4e
AdolfoCunquero <adolfocunquero.077@gmail.com>
2021-11-01 17:03:41 -0600

    first commit

---
+++ test/main_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"testing"
+)
+
+func Sum(x int, y int) int {
+	return x + y
+}
+
+func TestSum(t *testing.T) {
+	total := Sum(5, 5)
+	if total != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+
+	total2 := Sum(15, 5)
+	if total2 != 20 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total2, 10)
+	}
+}
+
+func TestSum2(t *testing.T) {
+	total := Sum(6, 5)
+	if total != 11 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+
+	total2 := Sum(5, 5)
+	if total2 != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total2, 10)
+	}
+}
